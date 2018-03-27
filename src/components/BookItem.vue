7c03a8e7c7a34225619a348095d518de5086f19e
than <seph.serg@gmail.com>
2018-03-27 23:14:12 +0800

    Added: BookItem component to represent Book

---
+++ src/components/BookItem.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="box-wrapper books">
+
+    <div class="box img flex-1">
+      <img :src="book.thumbnail" :alt="book.title" class="image">
+    </div>
+
+    <div class="box flex-2">
+      <h4>
+        <a :href="book.link">{{ book.title }}</a>
+      </h4>
+      <el-tag type="success">{{ title }}</el-tag>
+      <span class="price">{{ book.price }} {{ book.priceCurrency }}</span>
+    </div>
+
+    <div class="box flex-3">
+      <h2>介紹</h2>
+      <p>{{ book.about }}</p>
+    </div>
+
+  </div>
+</template>
+
+
+<script>
+  export default {
+    name: 'book-item',
+    props: ['title', 'book']
+  };
+
+</script>
+
+<style lang="scss" scoped>
+* body {
+  font-size: 14px;
+}
+
+a {
+  color: #2B81DA;
+  text-decoration: none;
+}
+
+h2 {
+  color: lighten(black, 20%);
+}
+
+p {
+  color: lighten(black, 35%);
+}
+
+.image {
+  width: 100%;
+  display: block;
+  flex: left;
+  border-radius: 5px;
+}
+
+.box-wrapper {
+  display: flex;
+  padding: 16px;
+
+  &.books {
+    border-bottom: 1px solid #d2d2d2;
+  }
+
+  &.center {
+    justify-content: center;
+    margin-top: 42px;
+  }
+
+  .box {
+    padding: 12px;
+    margin: 0 8px;
+
+    .price {
+      font-size: 1.5rem;
+      color: lighten(black, 20%);
+      margin-left: 8px;
+      padding: 0.5rem 1rem;
+    }
+  }
+}
+
+.search-btn {
+  margin: 0 12px;
+}
+
+.flex-1 {
+  flex: 1;
+}
+
+.flex-2 {
+  flex: 2;
+}
+
+.flex-3 {
+  flex: 3;
+}
+
+.flex-8 {
+  flex: 8;
+}
+
+
+</style>
--- src/components/EbookTable.vue
+++ src/components/EbookTable.vue
@@ -8,163 +8,75 @@
     <div class="box-wrapper center">
       <vue-spinner :loading="isLoading" color="#409EFF" size="45px" margin="24px"></vue-spinner>
     </div>
-    <div>
-      <div v-for="book in booksCompany" class="box-wrapper books">
 
-        <div class="box img flex-1">
-          <img :src="book.thumbnail" :alt="book.title" class="image">
-        </div>
+    <book-item v-for="(book, index) in booksCompany"
+      v-bind:title="'博客來'"
+      v-bind:book="book"
+      :index="index"
+      :key="book.id">
+    </book-item>
 
-        <div class="box flex-2">
-          <h4><a :href="book.link">{{ book.title }}</a></h4>
-          <el-tag type="success">博客來</el-tag>
-          <span class="price">{{ book.price }} {{ book.priceCurrency}}</span>
-        </div>
+    <book-item v-for="(book, index) in readmoo"
+      v-bind:title="'Readmoo 讀墨'"
+      v-bind:book="book"
+      :index="index"
+      :key="book.id">
+    </book-item>
 
-        <div class="box flex-3">
-          <h2>介紹</h2>
-          <p>{{ book.about }}</p>
-        </div>
+    <book-item v-for="(book, index) in kobo"
+      v-bind:title="'Rakuten kobo'"
+      v-bind:book="book"
+      :index="index"
+      :key="book.id">
+    </book-item>
 
-      </div>
-    </div>
+    <book-item v-for="(book, index) in taaze"
+      v-bind:title="'讀冊生活 TAAZE'"
+      v-bind:book="book"
+      :index="index"
+      :key="book.id">
+    </book-item>
 
-    <div>
-      <div v-for="book in readmoo" class="box-wrapper books">
+    <book-item v-for="(book, index) in bookWalker"
+      v-bind:title="'BOOKWALKER'"
+      v-bind:book="book"
+      :index="index"
+      :key="book.id">
+    </book-item>
 
-        <div class="box img flex-1">
-          <img :src="book.thumbnail" :alt="book.title" class="image">
-        </div>
+    <book-item v-for="(book, index) in playStore"
+      v-bind:title="'Google Play'"
+      v-bind:book="book"
+      :index="index"
+      :key="book.id">
+    </book-item>
 
-        <div class="box flex-2">
-          <h4><a :href="book.link">{{ book.title }}</a></h4>
-          <el-tag type="success">Readmoo 讀墨</el-tag>
-          <span class="price">{{ book.price }} {{ book.priceCurrency}}</span>
-        </div>
-
-        <div class="box flex-3">
-          <h2>介紹</h2>
-          <p>{{ book.about }}</p>
-        </div>
-
-      </div>
-    </div>
-
-    <div>
-      <div v-for="book in kobo" class="box-wrapper books">
-
-        <div class="box img flex-1">
-          <img :src="book.thumbnail" :alt="book.title" class="image">
-        </div>
-
-        <div class="box flex-2">
-          <h4><a :href="book.link">{{ book.title }}</a></h4>
-          <el-tag type="success">Rakuten kobo</el-tag>
-          <span class="price">{{ book.price }} {{ book.priceCurrency}}</span>
-        </div>
-
-        <div class="box flex-3">
-          <h2>介紹</h2>
-          <p>{{ book.about }}</p>
-        </div>
-
-      </div>
-    </div>
-
-    <div>
-      <div v-for="book in taaze" class="box-wrapper books">
-
-        <div class="box img flex-1">
-          <img :src="book.thumbnail" :alt="book.title" class="image">
-        </div>
-
-        <div class="box flex-2">
-          <h4><a :href="book.link">{{ book.title }}</a></h4>
-          <el-tag type="success">讀冊生活 TAAZE</el-tag>
-          <span class="price">{{ book.price }} {{ book.priceCurrency}}</span>
-        </div>
-
-        <div class="box flex-3">
-          <h2>介紹</h2>
-          <p>{{ book.about }}</p>
-        </div>
-
-      </div>
-    </div>
-
-    <div>
-      <div v-for="book in bookWalker" class="box-wrapper books">
-
-        <div class="box img flex-1">
-          <img :src="book.thumbnail" :alt="book.title" class="image">
-        </div>
-
-        <div class="box flex-2">
-          <h4><a :href="book.link">{{ book.title }}</a></h4>
-          <el-tag type="success">BOOKWALKER</el-tag>
-          <span class="price">{{ book.price }} {{ book.priceCurrency}}</span>
-        </div>
-
-        <div class="box flex-3">
-          <h2>介紹</h2>
-          <p>{{ book.about }}</p>
-        </div>
-
-      </div>
-    </div>
-
-    <div>
-      <div v-for="book in playStore" class="box-wrapper books">
-
-        <div class="box img flex-1">
-          <img :src="book.thumbnail" :alt="book.title" class="image">
-        </div>
-
-        <div class="box flex-2">
-          <h4><a :href="book.link">{{ book.title }}</a></h4>
-          <el-tag type="success">Google Play</el-tag>
-          <span class="price">{{ book.price }} {{ book.priceCurrency}}</span>
-        </div>
-
-        <div class="box flex-3">
-          <h2>介紹</h2>
-          <p>{{ book.about }}</p>
-        </div>
-
-      </div>
-    </div>
-
-    <div>
-      <div v-for="book in pubu" class="box-wrapper books">
-
-        <div class="box img flex-1">
-          <img :src="book.thumbnail" :alt="book.title" class="image">
-        </div>
-
-        <div class="box flex-2">
-          <h4><a :href="book.link">{{ book.title }}</a></h4>
-          <el-tag type="success">Pubu 電子書城</el-tag>
-          <span class="price">{{ book.price }} {{ book.priceCurrency}}</span>
-        </div>
-
-        <div class="box flex-3">
-          <h2>介紹</h2>
-          <p>{{ book.about }}</p>
-        </div>
-
-      </div>
-    </div>
+    <book-item v-for="(book, index) in playStore"
+      v-bind:title="'Google Play'"
+      v-bind:book="book"
+      :index="index"
+      :key="book.id">
+    </book-item>
+    
+    <book-item v-for="(book, index) in pubu"
+      v-bind:title="'Pubu 電子書城'"
+      v-bind:book="book"
+      :index="index"
+      :key="book.id">
+    </book-item>
 
   </el-main>
 </template>
 
 <script>
 import PacmanLoader from 'vue-spinner/src/PacmanLoader';
+import BookItem from './BookItem';
 
 export default {
   name: 'ebook-table',
   components: {
     'vue-spinner': PacmanLoader,
+    'book-item': BookItem 
   },
   data () {
     return {
@@ -209,26 +121,6 @@ export default {
   font-size: 14px;
 }
 
-a {
-  color: #2B81DA;
-  text-decoration: none;
-}
-
-h2 {
-  color: lighten(black, 20%);
-}
-
-p {
-  color: lighten(black, 35%);
-}
-
-.image {
-  width: 100%;
-  display: block;
-  flex: left;
-  border-radius: 5px;
-}
-
 .box-wrapper {
   display: flex;
   padding: 16px;
@@ -258,22 +150,5 @@ p {
 .search-btn {
   margin: 0 12px;
 }
-
-.flex-1 {
-  flex: 1;
-}
-
-.flex-2 {
-  flex: 2;
-}
-
-.flex-3 {
-  flex: 3;
-}
-
-.flex-8 {
-  flex: 8;
-}
-
 
 </style>
