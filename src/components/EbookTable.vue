0f2179b088a9ad8df05900ee843b953f6bb3079c
than <seph.serg@gmail.com>
2018-04-14 11:29:21 +0800

    refactor layout

--- src/components/EbookTable.vue
+++ src/components/EbookTable.vue
@@ -1,161 +1,166 @@
 <template>
-  <el-main>
-    <div style="margin-top: 15px;">
-      <el-input placeholder="搜尋您想比價的電子書名關鍵字" v-model="input" v-on:keyup.enter="submitSearch" class="input-with-select">
-        <el-button v-on:click="submitSearch" slot="append" icon="el-icon-search"></el-button>
-      </el-input>
-    </div>
-    <div class="box-wrapper center">
-      <vue-spinner :loading="isLoading" color="#409EFF" size="45px" margin="24px"></vue-spinner>
-    </div>
-    <div>
-      <div v-for="book in booksCompany" class="box-wrapper books">
-
-        <div class="box img flex-1">
-          <img :src="book.thumbnail" :alt="book.title" class="image">
-        </div>
-
-        <div class="box flex-2">
-          <h4><a :href="book.link">{{ book.title }}</a></h4>
-          <el-tag type="success">博客來</el-tag>
-          <span class="price">{{ book.price }} {{ book.priceCurrency}}</span>
-        </div>
-
-        <div class="box flex-3">
-          <h2>介紹</h2>
-          <p>{{ book.about }}</p>
-        </div>
-
+  <el-container>
+    <el-header>
+      <h3>台灣電子書搜尋</h3>
+    </el-header>
+    <el-main>
+      <div style="margin-top: 15px;">
+        <el-input placeholder="搜尋您想比價的電子書名關鍵字" v-model="input" v-on:keyup.enter="submitSearch" class="input-with-select">
+          <el-button v-on:click="submitSearch" slot="append" icon="el-icon-search"></el-button>
+        </el-input>
       </div>
-    </div>
-
-    <div>
-      <div v-for="book in readmoo" class="box-wrapper books">
-
-        <div class="box img flex-1">
-          <img :src="book.thumbnail" :alt="book.title" class="image">
-        </div>
-
-        <div class="box flex-2">
-          <h4><a :href="book.link">{{ book.title }}</a></h4>
-          <el-tag type="success">Readmoo 讀墨</el-tag>
-          <span class="price">{{ book.price }} {{ book.priceCurrency}}</span>
-        </div>
-
-        <div class="box flex-3">
-          <h2>介紹</h2>
-          <p>{{ book.about }}</p>
-        </div>
-
+      <div class="box-wrapper center">
+        <vue-spinner :loading="isLoading" color="#409EFF" size="45px" margin="24px"></vue-spinner>
       </div>
-    </div>
+      <div>
+        <div v-for="book in booksCompany" class="box-wrapper books">
 
-    <div>
-      <div v-for="book in kobo" class="box-wrapper books">
+          <div class="box img flex-1">
+            <img :src="book.thumbnail" :alt="book.title" class="image">
+          </div>
 
-        <div class="box img flex-1">
-          <img :src="book.thumbnail" :alt="book.title" class="image">
+          <div class="box flex-2">
+            <h4><a :href="book.link">{{ book.title }}</a></h4>
+            <el-tag type="success">博客來</el-tag>
+            <span class="price">{{ book.price }} {{ book.priceCurrency}}</span>
+          </div>
+
+          <div class="box flex-3">
+            <h2>介紹</h2>
+            <p>{{ book.about }}</p>
+          </div>
+
         </div>
-
-        <div class="box flex-2">
-          <h4><a :href="book.link">{{ book.title }}</a></h4>
-          <el-tag type="success">Rakuten kobo</el-tag>
-          <span class="price">{{ book.price }} {{ book.priceCurrency}}</span>
-        </div>
-
-        <div class="box flex-3">
-          <h2>介紹</h2>
-          <p>{{ book.about }}</p>
-        </div>
-
       </div>
-    </div>
 
-    <div>
-      <div v-for="book in taaze" class="box-wrapper books">
+      <div>
+        <div v-for="book in readmoo" class="box-wrapper books">
 
-        <div class="box img flex-1">
-          <img :src="book.thumbnail" :alt="book.title" class="image">
+          <div class="box img flex-1">
+            <img :src="book.thumbnail" :alt="book.title" class="image">
+          </div>
+
+          <div class="box flex-2">
+            <h4><a :href="book.link">{{ book.title }}</a></h4>
+            <el-tag type="success">Readmoo 讀墨</el-tag>
+            <span class="price">{{ book.price }} {{ book.priceCurrency}}</span>
+          </div>
+
+          <div class="box flex-3">
+            <h2>介紹</h2>
+            <p>{{ book.about }}</p>
+          </div>
+
         </div>
-
-        <div class="box flex-2">
-          <h4><a :href="book.link">{{ book.title }}</a></h4>
-          <el-tag type="success">讀冊生活 TAAZE</el-tag>
-          <span class="price">{{ book.price }} {{ book.priceCurrency}}</span>
-        </div>
-
-        <div class="box flex-3">
-          <h2>介紹</h2>
-          <p>{{ book.about }}</p>
-        </div>
-
       </div>
-    </div>
 
-    <div>
-      <div v-for="book in bookWalker" class="box-wrapper books">
+      <div>
+        <div v-for="book in kobo" class="box-wrapper books">
 
-        <div class="box img flex-1">
-          <img :src="book.thumbnail" :alt="book.title" class="image">
+          <div class="box img flex-1">
+            <img :src="book.thumbnail" :alt="book.title" class="image">
+          </div>
+
+          <div class="box flex-2">
+            <h4><a :href="book.link">{{ book.title }}</a></h4>
+            <el-tag type="success">Rakuten kobo</el-tag>
+            <span class="price">{{ book.price }} {{ book.priceCurrency}}</span>
+          </div>
+
+          <div class="box flex-3">
+            <h2>介紹</h2>
+            <p>{{ book.about }}</p>
+          </div>
+
         </div>
-
-        <div class="box flex-2">
-          <h4><a :href="book.link">{{ book.title }}</a></h4>
-          <el-tag type="success">BOOKWALKER</el-tag>
-          <span class="price">{{ book.price }} {{ book.priceCurrency}}</span>
-        </div>
-
-        <div class="box flex-3">
-          <h2>介紹</h2>
-          <p>{{ book.about }}</p>
-        </div>
-
       </div>
-    </div>
 
-    <div>
-      <div v-for="book in playStore" class="box-wrapper books">
+      <div>
+        <div v-for="book in taaze" class="box-wrapper books">
 
-        <div class="box img flex-1">
-          <img :src="book.thumbnail" :alt="book.title" class="image">
+          <div class="box img flex-1">
+            <img :src="book.thumbnail" :alt="book.title" class="image">
+          </div>
+
+          <div class="box flex-2">
+            <h4><a :href="book.link">{{ book.title }}</a></h4>
+            <el-tag type="success">讀冊生活 TAAZE</el-tag>
+            <span class="price">{{ book.price }} {{ book.priceCurrency}}</span>
+          </div>
+
+          <div class="box flex-3">
+            <h2>介紹</h2>
+            <p>{{ book.about }}</p>
+          </div>
+
         </div>
-
-        <div class="box flex-2">
-          <h4><a :href="book.link">{{ book.title }}</a></h4>
-          <el-tag type="success">Google Play</el-tag>
-          <span class="price">{{ book.price }} {{ book.priceCurrency}}</span>
-        </div>
-
-        <div class="box flex-3">
-          <h2>介紹</h2>
-          <p>{{ book.about }}</p>
-        </div>
-
       </div>
-    </div>
 
-    <div>
-      <div v-for="book in pubu" class="box-wrapper books">
+      <div>
+        <div v-for="book in bookWalker" class="box-wrapper books">
 
-        <div class="box img flex-1">
-          <img :src="book.thumbnail" :alt="book.title" class="image">
+          <div class="box img flex-1">
+            <img :src="book.thumbnail" :alt="book.title" class="image">
+          </div>
+
+          <div class="box flex-2">
+            <h4><a :href="book.link">{{ book.title }}</a></h4>
+            <el-tag type="success">BOOKWALKER</el-tag>
+            <span class="price">{{ book.price }} {{ book.priceCurrency}}</span>
+          </div>
+
+          <div class="box flex-3">
+            <h2>介紹</h2>
+            <p>{{ book.about }}</p>
+          </div>
+
         </div>
-
-        <div class="box flex-2">
-          <h4><a :href="book.link">{{ book.title }}</a></h4>
-          <el-tag type="success">Pubu 電子書城</el-tag>
-          <span class="price">{{ book.price }} {{ book.priceCurrency}}</span>
-        </div>
-
-        <div class="box flex-3">
-          <h2>介紹</h2>
-          <p>{{ book.about }}</p>
-        </div>
-
       </div>
-    </div>
 
-  </el-main>
+      <div>
+        <div v-for="book in playStore" class="box-wrapper books">
+
+          <div class="box img flex-1">
+            <img :src="book.thumbnail" :alt="book.title" class="image">
+          </div>
+
+          <div class="box flex-2">
+            <h4><a :href="book.link">{{ book.title }}</a></h4>
+            <el-tag type="success">Google Play</el-tag>
+            <span class="price">{{ book.price }} {{ book.priceCurrency}}</span>
+          </div>
+
+          <div class="box flex-3">
+            <h2>介紹</h2>
+            <p>{{ book.about }}</p>
+          </div>
+
+        </div>
+      </div>
+
+      <div>
+        <div v-for="book in pubu" class="box-wrapper books">
+
+          <div class="box img flex-1">
+            <img :src="book.thumbnail" :alt="book.title" class="image">
+          </div>
+
+          <div class="box flex-2">
+            <h4><a :href="book.link">{{ book.title }}</a></h4>
+            <el-tag type="success">Pubu 電子書城</el-tag>
+            <span class="price">{{ book.price }} {{ book.priceCurrency}}</span>
+          </div>
+
+          <div class="box flex-3">
+            <h2>介紹</h2>
+            <p>{{ book.about }}</p>
+          </div>
+
+        </div>
+      </div>
+
+    </el-main>
+  </el-container>
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -3,12 +3,14 @@
 import Vue from 'vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-import { Main, Tag, Input, Button, Icon } from 'element-ui'
+import { Main, Header, Container, Tag, Input, Button, Icon } from 'element-ui'
 import App from './App'
 
 Vue.config.productionTip = false
 
 Vue.component(Main.name, Main)
+Vue.component(Header.name, Header);
+Vue.component(Container.name, Container);
 Vue.component(Tag.name, Tag)
 Vue.component(Input.name, Input)
 Vue.component(Button.name, Button)
